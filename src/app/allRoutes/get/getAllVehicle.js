bd4f0c445d9fbf83002fe8149d59b3a2155da208
bagaskaramadhan <bagaskaramadhan@gmail.com>
2024-07-02 22:46:38 +0700

    Create entity filter

--- src/app/allRoutes/get/getAllVehicle.js
+++ src/app/allRoutes/get/getAllVehicle.js
@@ -5,13 +5,23 @@ const VehicleType = require("../../../model/modelVehicleType");
 
 exports.getAllVehicles = async (req, res) => {
     try {
-        let { page, limit } = req.query;
+        let { page, limit, name, type_id } = req.query;
         page = parseInt(page) || 1;
         limit = parseInt(limit) || 10;
+        const where = {};
+
+        if (name) {
+            where.name = { [Op.like]: `%${name}%` };
+        }
+
+        if (type_id) {
+            where.type_id = type_id;
+        }
 
         const queryOptions = {
+            where,
             include: [
-                { model: VehicleType, include: [VehicleBrand] }
+                { model: VehicleType, include: [VehicleBrand] },
             ],
             offset: (page - 1) * limit,
             limit: limit
@@ -21,16 +31,29 @@ exports.getAllVehicles = async (req, res) => {
         const totalCount = await VehicleModel.count(queryOptions);
         const totalPages = Math.ceil(totalCount / limit);
 
-        res.status(200).json({
-            data: vehicles,
-            metadata: {
-                totalItems: totalCount,
-                totalPages: totalPages,
-                currentPage: page,
-                itemsPerPage: limit
-            }
-        });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+        let result = [];
+        await vehicles.map((item) => {
+            result.push(
+                {
+                    id: item.dataValues.id,
+                    name: item.dataValues.name,
+                    type_id: item.dataValues.type_id,
+                    created_at: item.dataValues.created_at,
+                    updated_at: item.dataValues.updated_at,
+                }
+            );
+    });
+
+    res.status(200).json({
+        data: result,
+        metadata: {
+            totalItems: totalCount,
+            totalPages: totalPages,
+            currentPage: page,
+            itemsPerPage: limit
+        }
+    });
+} catch (err) {
+    res.status(500).json({ error: err.message });
+}
 };
